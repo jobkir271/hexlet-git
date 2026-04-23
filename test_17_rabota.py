1ece934d3d70a116b069e95b567acec32d9eac93
kirill morozov <jobkir271@gmail.com>
2026-04-23 11:55:47 +0700

    upadatefile2304

---
+++ test_17_rabota.py
@@ -0,0 +1,10 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_add_todo(page):
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+
+
